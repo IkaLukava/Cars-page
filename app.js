730d7b12592dc4d64de860c8b5d280a50f1ad0dd
Ika Lukava <iraklilukava06@gmail.com>
2023-11-25 11:55:08 +0400

    add js congrat

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const navbarLinks = document.getElementsByClassName('header-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
